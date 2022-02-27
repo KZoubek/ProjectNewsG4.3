243f19078721288f6f38365750131da5a409ebb9
KZoubek <katharina.zoubek@gmx.net>
2022-02-27 16:33:59 +0100

    added dummy functions according to exercise diagramm

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -1,4 +1,37 @@
 package at.ac.fhcampuswien;
 
+import java.util.List;
+
 public class AppController {
+    private List<Article> articles;
+
+    public AppController(List<Article> articles) {
+        this.articles = articles;
+        // hier SUPER klasse von Articles?
+    }
+
+    public void setArticles(List<Article> articles) {
+
+    }
+
+    public int getArticles(){
+
+    }
+
+    public List<Article> getTopHeadlinesAustria() {
+
+    }
+
+    public List<Article> getAllNewsBitcoin() {
+
+    }
+
+    private List<Article> filterList(String query, List<Article> articles){
+
+    }
+
+    private List<Article> generateMockList(){
+
+    }
+
 }
