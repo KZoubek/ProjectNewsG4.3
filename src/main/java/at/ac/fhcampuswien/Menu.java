bbe003791988fc3ea3c5f5403270a749675c93ab
1xmody <modytamim@gmail.com>
2022-03-02 11:15:27 +0100

    added dummy code for menu class

--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -2,4 +2,16 @@ package at.ac.fhcampuswien;
 
 public class Menu {
     //test
+    public void start(){
+
+    }
+    private void handeinput(String input){
+
+    }
+    private void getArtcleCount(AppController.controller){
+
+    }
+    private void getTopHeadlinesAustria(AppController.controller){
+
+    }
 }
