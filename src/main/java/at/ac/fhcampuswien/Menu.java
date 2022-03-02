a5215d135fc8131b4b6168f12360bf21d3c5a32e
KZoubek <katharina.zoubek@gmx.net>
2022-03-02 11:16:14 +0100

    added parameters

--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -2,7 +2,7 @@ package at.ac.fhcampuswien;
 
 public class Menu {
     private AppController controller = new AppController();
-    private String INVALD_INPUT_MESSAGE;
+    private String INVALID_INPUT_MESSAGE;
     private String EXIT_MESSAGE;
 
     public void start(){
