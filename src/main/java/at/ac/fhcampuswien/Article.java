4f0f06fd1d2ad0a6a21e50d18a1ef5b4cef0b06f
Julian Wildauer <max.kell@tuta.io>
2022-03-02 11:19:11 +0100

    Empty funktions

--- src/main/java/at/ac/fhcampuswien/Article.java
+++ src/main/java/at/ac/fhcampuswien/Article.java
@@ -2,4 +2,22 @@ package at.ac.fhcampuswien;
 
 public class Article {
     //new branch empty
+    String author;
+    String title;
+
+    public Article(String author,String title ){
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    /*public String toString(){
+        return String;
+    }*/
 }
