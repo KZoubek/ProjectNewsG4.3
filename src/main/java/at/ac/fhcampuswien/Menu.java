a94d9ab52d70d71ec9aabd675dd5a2b391f1b414
VjollcaB <71784141+VjollcaB@users.noreply.github.com>
2022-03-02 12:04:56 +0100

    edit for dummy function

--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -2,4 +2,14 @@ package at.ac.fhcampuswien;
 
 public class Menu {
     //test
+
+    private void getAllNewsBitcoin(AppController.controller){}
+
+    private static void printExitMessage(){}
+
+    private static void printInvalidInputMessage(){}
+
+    private static void printMenu(){}
+
+
 }
