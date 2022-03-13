be1b8f04921b1f59ea82f5382b190dbb8ca5e932
KZoubek <katharina.zoubek@gmx.net>
2022-03-13 16:30:07 +0100

    test written without execution

--- src/test/java/at/ac/fhcampuswien/appcontroller/setArticlesTest.java
+++ src/test/java/at/ac/fhcampuswien/appcontroller/setArticlesTest.java
@@ -1,35 +1,42 @@
 package at.ac.fhcampuswien.appcontroller;
 
+import at.ac.fhcampuswien.AppController;
+import at.ac.fhcampuswien.Article;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class setArticlesTest {
 
     @Test
-    public void setArticles1(){
+    public void setArticles1() {
         //create new object from Appcontroller to call a specific method
         AppController articletest = new AppController();
-        articletest.setArticles(ArrayList<Article>);
+        articletest.setArticles(List<Article>);
 
+        //Dummy list to have an expected
         List<Article> mock = new ArrayList<>();
         Article one = new Article("Margarete Schramboeck", "SWIFT geht in Austria auch mit Erlagschein");
         mock.add(one);
         Article two = new Article("Simon Neuch", "Angermanagement and Mandalas");
         mock.add(two);
-        Article three= new Article("Bloomberg", "How to: Bitcoin in echtes Geld umwandeln");
+        Article three = new Article("Bloomberg", "How to: Bitcoin in echtes Geld umwandeln");
         mock.add(three);
-        Article four= new Article("Dr.Acula", "Viertes Buch");
+        Article four = new Article("Dr.Acula", "Viertes Buch");
         mock.add(four);
-        Article five= new Article("five", "Fünftes Buch");
+        Article five = new Article("five", "Fünftes Buch");
         mock.add(five);
-        Article six= new Article("six", "Sechstes Buch");
+        Article six = new Article("six", "Sechstes Buch");
         mock.add(six);
-
-        //define where to save data
-        size.actual = articles.setArticles();
+        //vivi 7-9
+        //mody 10-12
+        return mock;
 
         // compare what we expect vs what we have actual
-        assertEquals(article, actual);
+        assertArrayEquals(mock, articletest.setArticles(List<Article>));
 
     }
 }
