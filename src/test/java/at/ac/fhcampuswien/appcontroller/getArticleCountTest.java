20d503ee8ac6f51fab45a76af2e1229751455419
KZoubek <katharina.zoubek@gmx.net>
2022-03-13 12:17:11 +0100

    wrote tests

--- src/test/java/at/ac/fhcampuswien/appcontroller/getArticleCountTest.java
+++ src/test/java/at/ac/fhcampuswien/appcontroller/getArticleCountTest.java
@@ -1,5 +1,32 @@
 package at.ac.fhcampuswien.appcontroller;
 
+import at.ac.fhcampuswien.Article;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class getArticleCountTest {
-    //test
-}
+
+    @Test
+    public void getArticleCount() {
+        List<Article> articles = new ArrayList<>();
+        int size = articles.size();
+        //Class<?> c = Class.forName("at.ac.fhcampuswien.AppController");
+
+        //keine Parameter
+        //Method m = c.getMethod("getArticleCount");
+        // hier speichern wir in den seize die "Eingabe" und rufen die Funktion auf
+        size.actual = articles.getArticleCount();
+
+        //Constructor<?> co1 = c.getConstructor(int.class, int.class, int.class, int.class);
+
+        articles = List<Article>;;
+        //Rectangle r1 = (Rectangle) co1.newInstance(2, 2, 4, 4);
+
+        assertEquals(size;"Count not correct.");
+    }
+    }
--- src/test/java/at/ac/fhcampuswien/appcontroller/setArticlesTest.java
+++ src/test/java/at/ac/fhcampuswien/appcontroller/setArticlesTest.java
@@ -1,5 +1,26 @@
 package at.ac.fhcampuswien.appcontroller;
 
+import at.ac.fhcampuswien.Article;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class setArticlesTest {
-    /
+
+    @Test
+    public void setArticles(){
+        //create new object
+        List<Article> articles = new ArrayList<>();
+        int size = articles.size();
+
+        //define where to save data
+        size.actual = articles.setArticles();
+
+        // compare what we expect vs what we have actual
+        assertEquals(article, actual);
+
+    }
 }
