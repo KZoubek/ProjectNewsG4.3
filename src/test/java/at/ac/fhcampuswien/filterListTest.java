a688a25b2051f457c7313a19f669a4fdf58869f1
KZoubek <katharina.zoubek@gmx.net>
2022-03-20 09:33:51 +0100

    written tests for 2x "the"

--- src/test/java/at/ac/fhcampuswien/filterListTest.java
+++ src/test/java/at/ac/fhcampuswien/filterListTest.java
@@ -50,6 +50,7 @@ public class filterListTest {
         austria.add(six);
         austria.add(eight);
 
+        //compare the two array Lists
         assertEquals(filterListT, austria);
 
     }
@@ -93,9 +94,10 @@ public class filterListTest {
         bitcoin.add(three);
         bitcoin.add(seven);
         bitcoin.add(ten);
-
+        //compare the two array Lists
         assertEquals(filterListT, bitcoin);
     }
+
     @Test //test for "mother"
     public void filterListTest3() {
         List<Article> mock3 = new ArrayList<>();
@@ -124,7 +126,7 @@ public class filterListTest {
         Article twelve = new Article("News Sky", "Mother who won 127,000 tells how she still ended up homeless");
         mock3.add(twelve);
 
-        // searching for "Austria" in Articles
+        // searching for "Mother" in Articles
         String query ="Mother";
 
         //create a new List where we can store the values
@@ -134,6 +136,50 @@ public class filterListTest {
         List<Article> mother = new ArrayList<>();
         mother.add(ten);
 
+        //compare the two array Lists
         assertEquals(filterListT, mother);
+    }
+    @Test //test for 2x "the"
+    public void filterListTest4() {
+        List<Article> mock3 = new ArrayList<>();
+        Article one = new Article("Margarete Schramboeck", "SWIFT geht in Austria auch mit Erlagschein");
+        mock3.add(one);
+        Article two = new Article("Simon Neuch", "Angermanagement and Mandalas");
+        mock3.add(two);
+        Article three= new Article("Bloomberg", "How to: Bitcoin in echtes Geld umwandeln");
+        mock3.add(three);
+        Article four= new Article("Dr.Acula", "Austria is too small for me");
+        mock3.add(four);
+        Article five= new Article("Hugh Jass", "Vienna Is On OnlyFans: Our Cultural Capital Gets Naked");
+        mock3.add(five);
+        Article six= new Article("Yuri Nator", "Erdäpfel, Topfen & Paradeiser are more than just words in Austria.");
+        mock3.add(six);
+        Article seven = new Article("Sova Nova", "Bitcoin soll in Zukunft gedruckt werden");
+        mock3.add(seven);
+        Article eight = new Article("Sage Smith", "Austria: Das A steht für Alpen");
+        mock3.add(eight);
+        Article nine = new Article("Raze Rammayer", "Krise: Schokoladen-Verbot in der EU");
+        mock3.add(nine);
+        Article ten = new Article("New York Times", "Eric Adams, a Bitcoin in Booster, Is Talking First Paycheck InCrypto");
+        mock3.add(ten);
+        Article eleven = new Article("News Sky", "Irishman held against his will in China for 3 years reunited with 'unbelievably happy' family");
+        mock3.add(eleven);
+        Article twelve = new Article("News Sky", "Mother who won 127,000 tells how she still ended up homeless");
+        mock3.add(twelve);
+        Article thirteen = new Article("Lola Free", "Why the is not the same as you might think");
+        mock3.add(thirteen);
+
+        // searching for "the" in Articles
+        String query ="the";
+
+        //create a new List where we can store the values
+        List<Article> filterListT = AppController.filterList(query, mock3);
+
+        //create a control list for comparing
+        List<Article> the = new ArrayList<>();
+        the.add(thirteen);
+
+        //compare the two array Lists
+        assertEquals(filterListT, the);
     }
 }
