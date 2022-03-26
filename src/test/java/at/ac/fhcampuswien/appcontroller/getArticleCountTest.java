1862a0937bfa063bf0a74ce09818c8459509ff99
1xmody <modytamim@gmail.com>
2022-03-26 15:13:59 +0100

    added failed getarticlecount test

--- src/test/java/at/ac/fhcampuswien/appcontroller/getArticleCountTest.java
+++ src/test/java/at/ac/fhcampuswien/appcontroller/getArticleCountTest.java
@@ -1,5 +1,23 @@
 package at.ac.fhcampuswien.appcontroller;
 
+import at.ac.fhcampuswien.AppController;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class getArticleCountTest {
     //test
+
+    @Test
+    public void getArticleCountTest() {
+
+        AppController myAppController = new AppController();
+
+        int expected = 12;
+        int actual = myAppController.getArticleCount();
+
+        assertEquals(expected, actual);
+
+    }
+
 }
