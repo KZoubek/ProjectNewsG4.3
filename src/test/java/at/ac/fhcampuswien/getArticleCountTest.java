bd14474998a173806d8b017b116dbd8e05371a80
*1xmody* <*modytamim@gmail.com*>
2022-03-30 13:50:56 +0200

    added comments to getArticleCountTest

--- src/test/java/at/ac/fhcampuswien/getArticleCountTest.java
+++ src/test/java/at/ac/fhcampuswien/getArticleCountTest.java
@@ -9,12 +9,16 @@ public class getArticleCountTest {
 
     @Test
     public void getArticleCountTest() {
-
+    //create an object of AppController
         AppController myAppController = new AppController();
 
+        // expected value is 12 because we have created 12 object in mocklist
         int expected = 12;
+
+        // get the actual value from method "getArticleCount"
         int actual = myAppController.getArticleCount();
 
+        // compare both values (expected and actual value)
         assertEquals(expected, actual);
 
     }
