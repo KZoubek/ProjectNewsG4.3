206a04b1f35ec47d4267956f797b999044319cef
KZoubek <katharina.zoubek@gmx.net>
2022-03-30 16:56:07 +0200

    corrected code after merging

--- src/test/java/at/ac/fhcampuswien/AppControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/AppControllerTest.java
@@ -10,8 +10,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppControllerTest {
 
+    /*
+    private static List<Article> mocklist() {
+        List<Article> mock = new ArrayList<>();
+        Article one = new Article("Margarete Schramboeck", "SWIFT geht in Austria auch mit Erlagschein");
+        mock.add(one);
+        Article two = new Article("Simon Neuch", "Angermanagement and Mandalas");
+        mock.add(two);
+        Article three = new Article("Bloomberg", "How to: Bitcoin in echtes Geld umwandeln");
+        mock.add(three);
+        Article four = new Article("Dr.Acula", "Austria is too small for me");
+        mock.add(four);
+        Article five = new Article("Hugh Jass", "Vienna Is On OnlyFans: Our Cultural Capital Gets Naked");
+        mock.add(five);
+        Article six = new Article("Yuri Nator", "Erdäpfel, Topfen & Paradeiser are more than just words in Austria.");
+        mock.add(six);
+        Article seven = new Article("Sova Nova", "Bitcoin soll in Zukunft gedruckt werden");
+        mock.add(seven);
+        Article eight = new Article("Sage Smith", "Austria: Das A steht für Alpen");
+        mock.add(eight);
+        Article nine = new Article("Raze Rammayer", "Krise: Schokoladen-Verbot in der EU");
+        mock.add(nine);
+        Article ten = new Article("New York Times", "Eric Adams, a Bitcoin in Booster, Is Talking First Paycheck InCrypto");
+        mock.add(ten);
+        Article eleven = new Article("News Sky", "Irishman held against his will in China for 3 years reunited with 'unbelievably happy' family");
+        mock.add(eleven);
+        Article twelve = new Article("News Sky", "Mother who won 127,000 tells how she still ended up homeless");
+        mock.add(twelve);
+        return mock;
+    }*/
+    
     @Test
-    @DisplayName("Test 1 for setArticle")
+    @DisplayName("Test 1 for setArticle with long list")
     public void setArticles1() {
 
         //Dummy list to have an expected
@@ -53,7 +83,7 @@ class AppControllerTest {
     }
 
     @Test
-    @DisplayName("Test 2 for setArticle")
+    @DisplayName("Test 2 for setArticle with long list")
     public void setArticles2() {
 
         //Dummy list to have an expected
